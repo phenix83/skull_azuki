98eceb2b0f67db7cb9919548b7018df6e09a6707
Dmytro Kryzhanovskyi <krdima83@gmail.com>
2022-08-20 21:22:31 +0200

    displaying cards correctly using js

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,48 @@ $(document).ready(function() {
     });
 
 });
+
+const cards = document.querySelectorAll('.sales_stages_card_wrapper .card'),
+      cardNumbers = document.querySelectorAll('.sales_stages_card_wrapper .card .number'),
+      cardArrows = document.querySelectorAll('.sales_stages_card_wrapper .arrow');
+
+console.log(cards);
+console.log(cardNumbers);
+console.log(cardArrows);
+
+
+// const addClassActive = () => {
+    cards.forEach((card, index) => {
+        card.addEventListener('click', function() {
+            card.classList.toggle('active');
+
+            cardNumbers[index].classList.toggle('number_active');
+
+            if (cardNumbers[index].innerHTML == '') {
+                console.log("index", index);
+                cardNumbers[index].innerHTML = index + 1;
+            } else {
+                cardNumbers[index].innerHTML = '';
+            }
+            
+            if (index < cardArrows.length) {
+                if (cardArrows[index].classList.contains('arrow_top')) {
+                    cardArrows[index].classList.toggle('white_top');                    
+                } else {
+                    cardArrows[index].classList.toggle('white_bottom');
+                }                
+            }           
+        })
+    })
+// }
+
+// addClassActive();
+
+
+// cards.addEventListener('click', function(e) {
+//     if (e.target.matches('.card')) {
+//         e.target.classList.toggle('active');
+//         let number = e.target.querySelector('.number');
+//         number.classList.toggle('number_active');
+//     }
+// });
